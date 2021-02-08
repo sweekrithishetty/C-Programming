4ac2c9fc24c5a40ec9c81940d064f2ad233b0e97
Sweekrithi  S  Shetty <shettysweekrithi03@gmail.com>
2021-02-08 17:58:01 +0530

    Add files via upload
    Problems  related  to Conditional loop

---
+++ ConditionProblems.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+//What is the output of the following C programme?
+//int main()
+//{
+    //Problem 1 (Conditional loop)
+//    int i = 1024;
+//    for(;i;i>>=1)
+//    printf("Hello World\n");
+//    return 0;
+
+    //Problem 2
+//    int i;
+//    for(i=0;i<20;i++)
+//    {
+//        switch(i)
+//        {
+//            case 0:i += 5;
+//           // break;
+//            case 1:i += 2;
+//           // break;
+//            case 5:i += 5;
+//            //break;
+//            default:i += 4;
+//
+//        }
+//        printf("%d ",i);
+
+
+//    }
+      //Problem 3 (How many times will the Programme will be printed)
+//      int i =-5;
+//      while(i<=5){
+//            if(i >=0)
+//                break;
+//            {
+//                i++;
+//                continue;
+//            }
+//            print("Programme");
+      //}
+
+      //Problem 4 (Assume size of unsigned  int is 4 bytes)(Output :1)
+//      unsigned int i =500;
+//      while(i++ !=0);
+//      printf("%d",i);
+//      return 0;
+
+    //Problem 5
+//        int x = 3;
+//        if(x == 2); x=0;
+//        if(x == 3) x++;
+//        else x += 2;
+//        printf("x = %d",x);
+//        return 0;
+//
+//      }
