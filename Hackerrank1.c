717231b92b7262f16e11360a4f7e8bbac08cc03b
Sweekrithi  S  Shetty <shettysweekrithi03@gmail.com>
2021-02-09 17:28:09 -0500

    Add files via upload

---
+++ Hackerrank1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+//int main()
+//{
+//    {
+//    char s[100];
+//    fgets(s, sizeof(s), stdin);
+//    printf("Hello, World!\n%s", s);
+//
+//    return 0;
+//
+
+
+//int a = 10,b=4;
+//    float c = 4.0f,d = 2.0f;
+//    int e = a + b;
+//    int f = b - a;
+//    int x = -(f);
+//    float h = c + d;
+//    float i = d - c;
+//    float y = -(i);
+//    printf("%d %d\n",e,x);
+//    printf("%.1f %.1f\n",h,y);
+//}
+
+
